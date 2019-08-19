5d7dd0dc826c54d1dcf36dee22d7060b654b4f79
Alix <dehaut.alix@gmail.com>
2019-08-19 15:23:00 +0200

    debug & asset test

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,16 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
 
     public function homepage()
     {
-        return new Response('Hommepage page test!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
